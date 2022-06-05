52b7625eac454f21b53a3fe0d54d43c7a603acf5
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-06-04 21:43:09 -0300

    feat: user data interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,17 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  it('should add user with complete data to mailing list', () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+
+    // const response = await usecase.registerUserOnMailingList({ name, email })
+    // const user = await repo.findUserByEmail(email)
+
+    // expect(user.name).toBe(name)
+  })
+})
